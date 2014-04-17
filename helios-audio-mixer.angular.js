1c1db3cd9ca7b2c1edacd13c19fafa8133f81da5
iainmcampbell <hey@iainmc.ca>
2014-04-17 17:35:08 -0400

    gain log

--- helios-audio-mixer.angular.js
+++ helios-audio-mixer.angular.js
@@ -16,98 +16,6 @@ var Mix,    // Mixer class
 
 
 
-// // Normalize prefixed implementations
-// // https://github.com/cwilso/AudioContext-MonkeyPatch/
-
-// function fixSetTarget(param) {
-//     if (!param) // if NYI, just return
-//         return;
-//     if (!param.setTargetAtTime)
-//         param.setTargetAtTime = param.setTargetValueAtTime; 
-// }
-
-// if (window.hasOwnProperty('webkitAudioContext') && !window.hasOwnProperty('AudioContext')) {
-
-//     window.AudioContext = webkitAudioContext;
-
-//     if (!AudioContext.prototype.hasOwnProperty('createGain'))
-//         AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
-//     if (!AudioContext.prototype.hasOwnProperty('createDelay'))
-//         AudioContext.prototype.createDelay = AudioContext.prototype.createDelayNode;
-//     if (!AudioContext.prototype.hasOwnProperty('createScriptProcessor'))
-//         AudioContext.prototype.createScriptProcessor = AudioContext.prototype.createJavaScriptNode;
-
-//     AudioContext.prototype.internal_createGain = AudioContext.prototype.createGain;
-//     AudioContext.prototype.createGain = function() { 
-//         var node = this.internal_createGain();
-//         fixSetTarget(node.gain);
-//         return node;
-//     };
-
-//     AudioContext.prototype.internal_createDelay = AudioContext.prototype.createDelay;
-//     AudioContext.prototype.createDelay = function(maxDelayTime) { 
-//         var node = maxDelayTime ? this.internal_createDelay(maxDelayTime) : this.internal_createDelay();
-//         fixSetTarget(node.delayTime);
-//         return node;
-//     };
-
-//     AudioContext.prototype.internal_createBufferSource = AudioContext.prototype.createBufferSource;
-//     AudioContext.prototype.createBufferSource = function() { 
-//         var node = this.internal_createBufferSource();
-//         if (!node.start) {
-//           node.start = function ( when, offset, duration ) {
-//               if ( offset || duration )
-//                 this.noteGrainOn( when, offset, duration );
-//               else
-//                 this.noteOn( when );
-//           }
-//         }
-//         if (!node.stop)
-//           node.stop = node.noteOff;
-//         fixSetTarget(node.playbackRate);
-//         return node;
-//     };
-
-//     AudioContext.prototype.internal_createDynamicsCompressor = AudioContext.prototype.createDynamicsCompressor;
-//     AudioContext.prototype.createDynamicsCompressor = function() { 
-//         var node = this.internal_createDynamicsCompressor();
-//         fixSetTarget(node.threshold);
-//         fixSetTarget(node.knee);
-//         fixSetTarget(node.ratio);
-//         fixSetTarget(node.reduction);
-//         fixSetTarget(node.attack);
-//         fixSetTarget(node.release);
-//         return node;
-//     };
-
-//     AudioContext.prototype.internal_createBiquadFilter = AudioContext.prototype.createBiquadFilter;
-//     AudioContext.prototype.createBiquadFilter = function() { 
-//         var node = this.internal_createBiquadFilter();
-//         fixSetTarget(node.frequency);
-//         fixSetTarget(node.detune);
-//         fixSetTarget(node.Q);
-//         fixSetTarget(node.gain);
-//         return node;
-//     };
-
-//     if (AudioContext.prototype.hasOwnProperty( 'createOscillator' )) {
-//         AudioContext.prototype.internal_createOscillator = AudioContext.prototype.createOscillator;
-//         AudioContext.prototype.createOscillator = function() { 
-//           var node = this.internal_createOscillator();
-//           if (!node.start)
-//               node.start = node.noteOn; 
-//           if (!node.stop)
-//               node.stop = node.noteOff;
-//           fixSetTarget(node.frequency);
-//           fixSetTarget(node.detune);
-//           return node;
-//         };
-//     }
-// }
-
-
-
-
 // ██████╗ ███████╗████████╗███████╗ ██████╗████████╗
 // ██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝
 // ██║  ██║█████╗     ██║   █████╗  ██║        ██║   
@@ -1511,6 +1419,8 @@ Track.prototype.gain = function(val){
                 this.nodes.gain.gain.value = this.options.gain * this.mix.gain;
             }       
         }
+
+        log('[Mixer] "'+this.name+'" setting gain to '+this.options.gain, 2)
 
         this.trigger('gain',this.options.gain, self);
 
